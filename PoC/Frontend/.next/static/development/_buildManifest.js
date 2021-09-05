bda883b3c3c9a447c6fe7f4e49c348f80049d1b2
Matheus da Silva Farias <71537027+MatthewsTomts@users.noreply.github.com>
2021-09-05 20:40:25 -0300

    Removendo .next/

--- PoC/Frontend/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/Cadastro":["static\u002Fchunks\u002Fpages\u002FCadastro.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FCadastro","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
