c5bbcd4768d50a979d144afe1d5cf6d4df1a6f13
FuentesRichard <richardfuentesrufino@gmail.com>
2021-09-09 14:58:09 -0300

    merge

---
+++ Projeto/backend/Day.Office/Day.Office.Api/Data/Repositories/IAgendamentoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Day.Office.Api.Data.Migrations;
+using System.Threading.Tasks;
+
+namespace Day.Office.Api.Data.Repositories
+{
+    public interface IAgendamentoRepository
+    {
+        Task<bool> AdicionarAgendamento(Agendamento agendamento);
+    }
+}
