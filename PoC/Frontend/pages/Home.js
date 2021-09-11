5fb5048a9b9a545e0b5ffcb5be8c020caf62b1a1
Matheus-Gondra <matheusgondra951@gmail.com>
2021-09-11 11:51:57 -0300

    Adicionando o conteúdo ao main

--- PoC/Frontend/pages/Home.js
+++ PoC/Frontend/pages/Home.js
@@ -20,25 +20,41 @@ const Cabecalho = styled.header`
 
 export default function Home() {
    return (
-      <Cabecalho>
-         <GlobalStyle/>
-         <img src=""></img>
-         <nav>
-            <ul>
-               <li>
-                  <a>Início</a>
-               </li>
-               <li>
-                  <a>Agendamento</a>
-               </li>
-               <li>
-                  <a>Conta</a>
-               </li>
-               <li>
-                  <a>Sair</a>
-               </li>
-            </ul>
-         </nav>
-      </Cabecalho>
+      <>
+         <Cabecalho>
+            <GlobalStyle />
+            <img src=""></img>
+            <nav>
+               <ul>
+                  <li>
+                     <a href="#">Início</a>
+                  </li>
+                  <li>
+                     <a href="#">Agendamento</a>
+                  </li>
+                  <li>
+                     <a href="#">Conta</a>
+                  </li>
+                  <li>
+                     <a href="#">Sair</a>
+                  </li>
+               </ul>
+            </nav>
+         </Cabecalho>
+
+         <main>
+            <div>
+               <h1>Meu Day Office</h1>
+               <input type="text" placeholder="Pesquisar meus eventos" />
+               <button>+ Criar</button>
+            </div>
+
+            <div>
+               <h2>Eventos</h2>
+               <h2>Histórico</h2>
+            </div>
+
+         </main>
+      </>
    );
 }
